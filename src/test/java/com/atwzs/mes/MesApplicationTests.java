45036f498225812fee2897fd4a668af3d616638a
Ricky <3517139394@qq.com>
2023-10-24 10:18:36 +0800

    11

---
+++ src/test/java/com/atwzs/mes/MesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atwzs.mes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
